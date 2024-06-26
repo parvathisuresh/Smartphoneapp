2de2bfeba9689617007f9aa4b16482695d83cf77
parvathisuresh <parvathisuresh0220@gmail.com>
2024-06-26 13:27:33 +0530

    smartphoneapp

---
+++ src/components/AddSmartphone.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+
+const AddSmartphone = () => {
+  return (
+    <div>
+      <div className="container">
+        <div className="row">
+            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <div className="row g-3">
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label htmlFor="" className="form-label">BrandName</label>
+                        <select name="" id="" className="form-control">
+                            <option value="Samsung">samsung</option>
+                            <option value="Nokia">Nokia</option>
+                            <option value="Apple">Apple</option>
+                            <option value="Android">Android</option>
+                            <option value="Real me">Real me</option>
+                        </select>
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label htmlFor="" className="form-label">Mobile Name</label>
+                        <input type="text" className="form-control" />
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label htmlFor="" className="form-label">Manufacturing Year</label>
+                        <input type="date" name="" id="" className="form-control" />
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label htmlFor="" className="form-label">Price</label>
+                        <input type="text" className="form-control" />
+                    </div>
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <label htmlFor="" className="form-label">Discription</label>
+                        <textarea name="" id="" className="form-control"></textarea>
+                    </div>
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <label htmlFor="" className="form-label">Image</label>
+                        <input type="file" name="" id="" className="form-control" />
+                    </div>
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <button className="btn btn-success">Add</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default AddSmartphone
