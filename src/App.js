4b5c9f9b70a2705d4b13548b72def7fe2edd3063
parvathisuresh <parvathisuresh0220@gmail.com>
2024-06-26 15:27:13 +0530

    SmartphoneApp

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import AddSmartphone from './components/AddSmartphone';
+import SearchSmartphone from './components/SearchSmartphone';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div> 
-    z<AddSmartphone/>
-    
-    </div>
+   <BrowserRouter>
+   <Routes>
+<Route path="/"element={<AddSmartphone/>}/>   
+<Route path="/search"element={<SearchSmartphone/>}/>   
+</Routes>
+   </BrowserRouter>
+
+  
   );
 }
 
